d73d597507bb8d91ea4f32e36b4c5dc22bc9229d
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-19 11:31:19 -0500

    server set up to handle express and next endpoint

--- server/index.js
+++ server/index.js
@@ -1,18 +1,10 @@
-// const { Pool } = require("pg");
 const express = require("express");
 const morgan = require("morgan");
+const next = require("next");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
-
-// Connect with PostgreSQL
-// const db = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "postgres",
-//   password: 456654,
-//   port: 5433,
-// });
+const dev = process.env.NODE_ENV !== "production";
 
 // Initialize Express app
 const app = express();
@@ -33,9 +25,19 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || "Internal server error.");
 });
 
-// Start Express server
-app.listen(PORT, () => {
-  console.log(`Listening on port http://localhost:${PORT}`);
+// Set up Next.js
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+nextApp.prepare().then(() => {
+  app.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  // Start Express server
+  app.listen(PORT, () => {
+    console.log(`Listening on port http://localhost:${PORT}`);
+  });
 });
 
 // module.exports = { db };
