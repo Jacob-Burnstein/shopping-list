de62c2e55f94e4eb2827b5f33ed44facafb14017
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-25 13:36:33 -0500

    prevent refresh on add store

--- app/components/AddStore.tsx
+++ app/components/AddStore.tsx
@@ -13,7 +13,8 @@ const AddStore: React.FC<AddStoreProps> = ({ addNewStore }) => {
     setStoreName(e.target.value);
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
     const storeToAdd: Store = {
       StoreName: storeName,
       UserId: 0,
@@ -39,9 +40,10 @@ const AddStore: React.FC<AddStoreProps> = ({ addNewStore }) => {
 
   return (
     <>
-      <form onSubmit={() => handleSubmit()}>
+      <form onSubmit={handleSubmit}>
         <label>Add Store:</label>
         <input type="text" value={storeName || ""} onChange={handleChange} />
+        <button type="submit">Add</button>
       </form>
     </>
   );
