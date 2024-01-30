e74c7629a87b307e34f9d98aa4d187b4d70dcca1
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-30 09:22:21 -0500

    right aligned nav bar links, still with formatting issues

--- app/components/header/NavBar.tsx
+++ app/components/header/NavBar.tsx
@@ -1,38 +1,48 @@
 "use client";
 
-import React from "react";
-import { usePathname } from "next/navigation";
+import React, { useState } from "react";
 import Link from "next/link";
 
 const NavBar = () => {
-  // const pathname = usePathname();
-  // const splitPathname = pathname.split("/");
-  // const username: string = splitPathname[splitPathname.length - 1];
-
   const token = localStorage.getItem("token");
-  // const username = localStorage.getItem("username");
+  const username = localStorage.getItem("username");
+  const [navClick, setNavClick] = useState<boolean>(false);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("username");
   };
 
+  const handleNavClick = (e: React.MouseEvent<HTMLElement>) => {
+    setNavClick(!navClick);
+  };
+
   return (
     <>
-      <nav className="links">
-        <Link href="/">Home</Link>
-        {!token ? (
-          <>
-            <Link href="/pages/login">Log In</Link>
-            <Link href="/pages/register">Create Account</Link>
-          </>
-        ) : (
-          <Link href="/" onClick={handleLogout}>
-            Log Out
-          </Link>
-        )}
-      </nav>
-      <span className="material-symbols-outlined">menu</span>
+      <section className="navBar cursor-pointer z-10 border">
+        <span
+          className="material-symbols-outlined transform scale-150 relative"
+          onClick={handleNavClick}
+        >
+          menu
+        </span>
+        <nav className={navClick ? "z-20" : "hidden"}>
+          {!token ? (
+            <nav className="linksContainer">
+              <Link href="/">Home</Link>
+              <Link href="/pages/login">Log In</Link>
+              <Link href="/pages/register">Create Account</Link>
+            </nav>
+          ) : (
+            <nav className="linksContainer flex flex-col">
+              <Link href={`/pages/user/${username}`}>My Stores</Link>
+              <Link href="/" onClick={handleLogout}>
+                Log Out
+              </Link>
+            </nav>
+          )}
+        </nav>
+      </section>
     </>
   );
 };
