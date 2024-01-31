d83f37eb3d7bae96a6a198c982c3a64e72e01241
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-31 16:33:41 -0500

    created new backend endpoints for get stores and add store - hardcoded for now

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,20 +1,43 @@
-import { NextApiResponse, NextApiRequest } from "next";
-import prisma from "../../../prisma/prisma/prisma";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "../../../prisma/index";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(
+  req: Request | NextRequest,
+  res: Response | NextResponse
+) {
+  // if (req.method === "GET") {
   try {
-    if (req.method === "GET") {
-      const stores = await prisma.store.findMany({
-        where: { UserId: 1 },
-      });
-      console.log("stores");
-      res.json(stores);
-    } else {
-      res.status(405).json({ error: "Method Not Allowed!" });
-    }
+    const stores = await prisma.store.findMany({
+      where: { UserId: 1 },
+    });
+    console.log("response: ", res);
+    return NextResponse.json(stores);
+
+    // else console.log(": (");
+  } catch (err) {
+    console.error(err);
+    NextResponse.json({ err: "Internal Server Error" }), { status: 500 };
+  }
+  // }
+  // else {
+  //   console.log("Invalid request!");
+  // }
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  // const { storeName } = req.body;
+  const testName = "Chuckiest Cheese";
+  try {
+    const newStore = await prisma.store.create({
+      data: { StoreName: testName, UserId: 1 },
+    });
+    return NextResponse.json(newStore);
   } catch (err) {
     console.error(err);
     return NextResponse.json(err);
   }
 }
+
+// export default {
+//   getHandler,
+// };
