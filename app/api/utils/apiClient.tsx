73e449f11673437cecb86592c1c1248847a00426
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-01 10:35:46 -0500

    log in working on front and backened

--- app/api/utils/apiClient.tsx
+++ app/api/utils/apiClient.tsx
@@ -10,25 +10,25 @@ const baseURL = isDevelopment ? "http://localhost:3000/api" : "/api";
 const apiClient = axios.create({
   baseURL,
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "multipart/form-data",
   },
 });
 
-apiClient.interceptors.request.use(
-  (config) => {
-    const authContext = useAuth();
-    const token = getAuthToken(authContext);
-    console.log("Token from APICLIENT:", token);
+// apiClient.interceptors.request.use(
+//   (config) => {
+//     const authContext = useAuth();
+//     const token = getAuthToken(authContext);
+//     console.log("Token from APICLIENT:", token);
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
 
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default apiClient;
