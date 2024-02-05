a032de9595955baf403abaedd2aa8de252de0fb6
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-05 16:21:48 -0500

    UI message for unauthenticated page access

--- app/pages/store/[storename]/page.tsx
+++ app/pages/store/[storename]/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ItemList from "../../../components/items/ItemList";
+import getTokenInfo from "../../../utils/getTokenInfo";
 
 const StorePage = () => {
+  const tokenInfo = getTokenInfo();
+
   return (
     <>
       <main>
-        <ItemList />
+        {tokenInfo ? (
+          <ItemList />
+        ) : (
+          <p>You must be logged in to view this page.</p>
+        )}
       </main>
     </>
   );
--- app/pages/user/[username]/page.tsx
+++ app/pages/user/[username]/page.tsx
@@ -1,11 +1,18 @@
 import StoreList from "../../../components/store/StoreList";
+import getTokenInfo from "../../../utils/getTokenInfo";
 
 const UserPage = () => {
+  const tokenInfo = getTokenInfo();
+
   return (
     <>
       <h1 className="text-xl font-semibold pb-2">Your Stores</h1>
       <main>
-        <StoreList />
+        {tokenInfo ? (
+          <StoreList />
+        ) : (
+          <p>You must be logged in to view this page.</p>
+        )}
       </main>
     </>
   );
