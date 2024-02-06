3e1056cbdfe696537431ee85f0516a2335855acc
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-06 12:46:56 -0500

    reformatting favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
-        <link href="../favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={`${quicksand.className}`}>
         <NavBar />
